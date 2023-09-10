0fee49f676b814377eddf1ae933e06d0bfd46d68
AbrarFaizMohammed <abrarfaizmohammed@gmail.com>
2023-09-09 23:03:16 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/abrarfaizmohammed.github.io",
+  base: "/amazonclone-deploy/",
   plugins: [react()],
 })
